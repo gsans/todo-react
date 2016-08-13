056ee93a9c416b2727e5d4a46bbcfa8f8a1ee468
Gerard Sans <gerard.sans@bigsystemstechnology.com>
2016-08-13 16:56:41 +0100

    Refactors

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,19 +3,17 @@ import Todo from './Todo'
 
 export default class TodoList extends React.Component {
 
-  _filterTodos = (todo) => (
-    this.props.filter === 'SHOW_ACTIVE'
-    ? todo.complete !== true
-    : this.props.filter === 'SHOW_COMPLETED'
-      ? todo.complete === true
-      : true
-    )
+  _filterTodos = todo => (
+    this.props.filter === 'SHOW_ALL' ||
+    this.props.filter === 'SHOW_ACTIVE' && !todo.complete ||
+    this.props.filter === 'SHOW_COMPLETED' && todo.complete
+  )
 
   renderTodos () {
     return this.props.todos
       .filter(this._filterTodos)
       .reverse()
-      .map((todo) =>
+      .map(todo =>
         <Todo
           key={todo.id}
           todo={todo}
