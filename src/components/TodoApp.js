ddba280e1ec7e55b1c17725f32beb82d19f248ac
James Baxley <james.baxley@newspring.cc>
2016-08-15 21:11:53 -0400

    update to new api

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { connect } from 'react-apollo'
+import { graphql } from 'react-apollo'
+import { connect } from 'react-redux'
 import gql from 'graphql-tag'
 
 import AddTodo from './AddTodo'
@@ -11,11 +12,11 @@ class TodoApp extends React.Component {
   render () {
     return (
       <div>
-        <AddTodo addTodo={this.props.mutations.addTodo} />
+        <AddTodo addTodo={this.props.addTodo} />
         <TodoList
-          todos={this.props.todos.allTodoes || []}
+          todos={this.props.data.allTodoes || []}
           filter={this.props.currentFilter}
-          toggleTodo={this.props.mutations.toggleTodo}
+          toggleTodo={this.props.toggleTodo}
         />
         <Filters setFilter={this.props.setFilter} filter={this.props.currentFilter} />
       </div>
@@ -23,65 +24,65 @@ class TodoApp extends React.Component {
   }
 }
 
-const TodoAppLinked = connect({
-  // currentFilter state
-  mapDispatchToProps (dispatch) {
-    return {
-      setFilter: (filter) => {
-        dispatch({
-          type: 'SET_FILTER',
-          filter,
-        })
-      },
+// currentFilter state
+function mapDispatchToProps (dispatch) {
+  return {
+    setFilter: (filter) => {
+      dispatch({
+        type: 'SET_FILTER',
+        filter,
+      })
+    },
+  }
+};
+
+function mapStateToProps (state) {
+  return {
+    currentFilter: state.filter,
+  }
+}
+
+const withRedux = connect(mapStateToProps, mapDispatchToProps)
+const withTodos = graphql(gql`
+    query todos {
+      allTodoes {
+        id
+        complete
+        text
+      }
     }
-  },
-  mapStateToProps (state) {
-    return {
-      currentFilter: state.filter,
-    }
-  },
-  // GraphQL
-  mapQueriesToProps () {
-    return {
-      todos: {
-        query: gql`
-          query todos {
-            allTodoes {
-              id
-              complete
-              text
-            }
-          }
-        `,
-        forceFetch: true,
-      },
-    }
-  },
-  mapMutationsToProps () {
-    return {
-      addTodo: (text) => ({
-        mutation: gql`
-          mutation addTodo($text: String!) {
-            createTodo(text: $text, complete: false) { id }
-          }`,
+  `, {
+    options: () => ({ forceFetch: true }),
+  }
+)
+
+const withAddTodo = graphql(gql`
+  mutation addTodo($text: String!) {
+    createTodo(text: $text, complete: false) { id }
+  }`, {
+    props: ({ mutate }) => ({
+      addTodo: (text) => mutate({
         variables: { text },
         updateQueries: {
-          todos: (state, { mutationResult }) => {
-            return {
-              allTodoes: [...state.allTodoes, {
-                id: mutationResult.data.createTodo.id,
-                text: text,
-                complete: false,
-              }],
-            }
-          },
+          todos: (state, { mutationResult }) => ({
+            allTodoes: [...state.allTodoes, {
+              id: mutationResult.data.createTodo.id,
+              text: text,
+              complete: false,
+            }],
+          }),
         },
       }),
+    }),
+  }
+)
+
+const withToggleTodo = graphql(gql`
+  mutation toggleTodo($id: ID!, $complete: Boolean!) {
+    updateTodo(id: $id, complete: $complete) { id complete }
+  }`, {
+    props: ({ mutate }) => ({
       toggleTodo: (id, complete) => ({
-        mutation: gql`
-          mutation toggleTodo($id: ID!, $complete: Boolean!) {
-            updateTodo(id: $id, complete: $complete) { id complete }
-          }`,
         variables: {
           id,
           complete,
@@ -90,7 +91,7 @@ const TodoAppLinked = connect({
           todos: (state, { mutationResult }) => {
             return {
               allTodoes: state.allTodoes.map(t => {
-                if (t.id===id) {
+                if (t.id === id) {
                   return {
                     id: t.id,
                     text: t.text,
@@ -103,8 +104,10 @@ const TodoAppLinked = connect({
           },
         },
       }),
-    }
-  },
-})(TodoApp)
+    }),
+  }
+)
+
+const TodoAppLinked = withRedux(withTodos(withAddTodo(withToggleTodo(TodoApp))))
 
 export default TodoAppLinked
